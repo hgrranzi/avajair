4019a9b77cd98608673d715666600056d54465fc
hgrranzi <uteena@student.42.fr>
2024-02-08 11:04:25 +0100

    avajair - provider and factory classes done

--- src/main/java/com/hgrranzi/avajair/AircraftFactory.java
+++ src/main/java/com/hgrranzi/avajair/AircraftFactory.java
@@ -1,4 +1,17 @@
 package com.hgrranzi.avajair;
 
 public class AircraftFactory {
+
+    public Flyable newAircraft(String p_type, String p_name, Coordinates p_coordinates) {
+        switch (p_type) {
+            case "Helicopter":
+                return new Helicopter(p_name, p_coordinates);
+            case "JetPlane":
+                return new JetPlane(p_name, p_coordinates);
+            case "Baloon":
+                return new Baloon(p_name, p_coordinates);
+            default:
+                throw new RuntimeException(String.format("Cannot create an aircraft of type: %s", p_type));
+        }
+    }
 }
--- src/main/java/com/hgrranzi/avajair/WeatherProvider.java
+++ src/main/java/com/hgrranzi/avajair/WeatherProvider.java
@@ -1,4 +1,12 @@
 package com.hgrranzi.avajair;
 
 public class WeatherProvider {
+
+    private String weather;
+
+    WeatherProvider() {}
+
+    public String getCurrentWeather(Coordinates p_coordinates) {
+        return weather;
+    }
 }
