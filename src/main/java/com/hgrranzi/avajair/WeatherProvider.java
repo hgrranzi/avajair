fc8684714caaad9495f1c9faef30fa0cfd3020a7
hgrranzi <vkh.mariia@gmail.com>
2024-02-08 18:46:01 +0100

    avajair - WeatherProvider class updated

--- src/main/java/com/hgrranzi/avajair/WeatherProvider.java
+++ src/main/java/com/hgrranzi/avajair/WeatherProvider.java
@@ -1,12 +1,25 @@
 package com.hgrranzi.avajair;
 
+import java.util.Random;
+
+// Singleton class
 public class WeatherProvider {
 
-    private String weather;
+    private static WeatherProvider instance = null;
 
-    WeatherProvider() {}
+    private static final String[] weather = {"RAIN", "FOG", "SUN", "SNOW"};
+
+    private WeatherProvider() {
+    }
+
+    public WeatherProvider getProvider() {
+        if (instance == null) {
+            instance = new WeatherProvider();
+        }
+        return instance;
+    }
 
     public String getCurrentWeather(Coordinates p_coordinates) {
-        return weather;
+        return weather[new Random().nextInt(4)];
     }
 }
