1c45740f498be5016275df9f04795b3e1f9fc9d4
hgrranzi <uteena@student.42.fr>
2024-02-08 10:57:20 +0100

    avajair - Tower class done

--- src/main/java/com/hgrranzi/avajair/Tower.java
+++ src/main/java/com/hgrranzi/avajair/Tower.java
@@ -1,4 +1,20 @@
 package com.hgrranzi.avajair;
 
+import java.util.List;
+
 public class Tower {
+
+    private List<Flyable> observers;
+
+    public void register(Flyable p_flyable) {
+
+    }
+
+    public void unregister(Flyable p_flyable) {
+
+    }
+
+    public void conditionChanged() {
+
+    }
 }
--- src/main/java/com/hgrranzi/avajair/WeatherTower.java
+++ src/main/java/com/hgrranzi/avajair/WeatherTower.java
@@ -1,4 +1,13 @@
 package com.hgrranzi.avajair;
 
 public class WeatherTower extends Tower {
+
+    public String getWeather(Coordinates p_coordinated) {
+        return "";
+    }
+
+    public void changeWeather() {
+
+    }
+
 }
